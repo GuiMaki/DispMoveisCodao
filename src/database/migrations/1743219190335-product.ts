fbbfc41be4608c507d752b9cda28e961776736f4
Guilherme Makiyama <makiyamagui@gmail.com>
2025-03-29 00:33:43 -0300

    create migrations

---
+++ src/database/migrations/1743219190335-product.ts
@@ -0,0 +1,14 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class product1743219190335 implements MigrationInterface {
+    name = 'product1743219190335'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "categories" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, CONSTRAINT "PK_24dbc6126a28ff948da33e97d3b" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "categories"`);
+    }
+
+}
