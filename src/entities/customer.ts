9e948c6d3f26739aaee5531cad93b3ae86830f91
Guilherme Makiyama <makiyamagui@gmail.com>
2025-03-29 00:08:24 -0300

    create customer entity

---
+++ src/entities/customer.ts
@@ -0,0 +1,37 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+@Entity("customers")
+class Customer {
+    @PrimaryGeneratedColumn("uuid")
+    readonly id!: string;
+
+    @Column()
+    name!: string;
+
+    @Column()
+    number!: string;
+
+    @Column()
+    email!: string;
+
+    @Column()
+    street!: string;
+
+    @Column()
+    neighborhood!: string;
+
+    @Column()
+    city!: string;
+
+    @Column()
+    state!: string;
+
+    constructor() {
+        if (!this.id) {
+            this.id = uuid();
+        }
+    }
+}
+
+export { Customer };
